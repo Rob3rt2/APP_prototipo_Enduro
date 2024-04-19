bfd4010ca00b42039e4b6adaa85d17ab6ef74a7d
Rob3rt2 <robert.mota@etec.sp.gov.br>
2024-04-19 08:34:59 -0300

    adicionando script

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AppEnduro (),
+    );
+  }
+}
+  class AppEnduro extends StatelessWidget{
+    const AppEnduro({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+
+      
+      )
+    )
+  }
+  }
+        
+       
